7de3ff53317e557f77b3130a5efa2dcc49dbd65c
pankaj0608 <pakr0608@gmail.com>
2015-07-12 21:37:29 -0400

    all

--- routes/articles.js
+++ routes/articles.js
@@ -38,6 +38,64 @@ router.get('/category/:category', function (req, res, next) {
     });
 });
 
+/* Create articles */
+router.post('/', function (req, res, next) {
+    var title = req.body.title;
+    var category = req.body.category;
+    var body = req.body.body;
 
+    var newArticle = new Article({
+        title: title,
+        category: category,
+        body: body
+    });
+
+    Article.createArticle(newArticle, function (err, article) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.location('/articles');
+            res.redact('/articles');
+        }
+    })
+});
+
+
+/* Update articles */
+router.put('/', function (req, res, next) {
+    var id = req.body.id;
+    var data = {
+        title: req.body.title,
+        category: req.body.category,
+        body: req.body.body
+    }
+
+    Article.updateArticle(id, data, function (err, article) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.location('/articles');
+            res.redact('/articles');
+        }
+    })
+});
+
+
+/* Delete articles */
+router.delete('/:id', function (req, res, next) {
+    var id = req.params.id;
+
+    Article.removeArticle(id, function (err, article) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.location('/articles');
+            res.redact('/articles');
+        }
+    })
+});
 
 module.exports = router;
